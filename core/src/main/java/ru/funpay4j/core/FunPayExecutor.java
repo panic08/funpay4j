e93eaeb7ec91c74aba253420c6e3939304d17b80
Andrey <andrey1010102008@gmail.com>
2024-10-02 21:32:25 +0300

    Added proxy constructor in FunPayExecutor

--- core/src/main/java/ru/funpay4j/core/FunPayExecutor.java
+++ core/src/main/java/ru/funpay4j/core/FunPayExecutor.java
@@ -14,6 +14,7 @@ import ru.funpay4j.core.objects.lot.Lot;
 import ru.funpay4j.core.objects.offer.Offer;
 import ru.funpay4j.core.objects.user.User;
 
+import java.net.Proxy;
 import java.util.List;
 
 /**
@@ -31,7 +32,11 @@ public class FunPayExecutor {
         this.funPayParser = new JsoupFunPayParser(httpClient);
     }
 
-    public FunPayExecutor(OkHttpClient httpClient) {
+    public FunPayExecutor(@NonNull Proxy proxy) {
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .proxy(proxy)
+                .build();
+
         this.funPayParser = new JsoupFunPayParser(httpClient);
     }
 
